5c2c34d2003e2f0abeceab932d85b0e6dbcbc245
nn43 <nn43.njit.edu>
2021-11-04 16:10:43 -0400

    FIXED ONE ERROR

--- calc/calculation.py
+++ calc/calculation.py
@@ -1,15 +1,26 @@
 """This is our calculation base class / Abstract Class"""
 class Calculation:
-    """Class calculation"""
-    #contstructor and it is the first function called when an object of the class is instantiated
-    def __init__(self,value_a, value_b):
-        #self references the instantiated object of the class
-        #these are instance properties that are being
-        # shared with the child classes (addition, subtraction, etc...)
-        self.value_a = value_a
-        self.value_b = value_b
+    """Initializing objects"""
+
+    # self references the instantiated object of the class
+    # these are instance properties that are being sharred
+    # with the child classes (addition, subtraction, et
+    def __init__(self, value_a, value_b):
+        self._value_a = value_a
+        self._value_b = value_b
+
     # Class Factory Method <- bound to the class and not the instance of the class
     @classmethod
     def create(cls, value_a, value_b):
-        """Base class result"""
+        """Using class method to create objects of all individual operations"""
         return cls(value_a,value_b)
+
+    @property
+    def value_a(self):
+        """Getter For Value A"""
+        return self._value_a
+
+    @property
+    def value_b(self):
+        """Getter For Value B"""
+        return self._value_b
