6c72af127b11bb0a93373df08767530e99e74bf5
nemanenikita <nn43@njit.edu>
2021-11-21 21:04:03 -0500

    Task: added subtraction test

---
+++ tests/subtraction_test.py
@@ -0,0 +1,20 @@
+"""Testing Addition"""
+import os
+
+import pandas as pd
+
+from calc.calculations.subtraction import Subtraction
+
+BASE_DIR = os.path.dirname(os.path.realpath(__file__)) # Get Current Working Directory
+
+
+def test_calculation_addition():
+    """testing that our calculator has a static method for addition"""
+    #Arrange
+    path = os.path.join(BASE_DIR,"subtraction2.xlsx")
+    df = pd.read_excel(path, names=["value1", "value2", "result"])
+    for index, row in df.iterrows():
+        values = (row.value1, row.value2)
+        subtraction= Subtraction.create(values)
+        assert subtraction.get_result() == df["result"][index]
+
